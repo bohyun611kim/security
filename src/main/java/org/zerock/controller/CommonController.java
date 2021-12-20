ced44adc9ba14ab8e52e831d33979b736a05ddbd
tomdanbi <bohyun611.kim@gmail.com>
2021-12-20 13:57:14 +0900

    커스텀 로그인 작업 02

--- src/main/java/org/zerock/controller/CommonController.java
+++ src/main/java/org/zerock/controller/CommonController.java
@@ -18,4 +18,20 @@ public class CommonController {
     model.addAttribute("msg", "이 페이지는 권한없는 사용자가 접근할 수 없습니다.");
   }
 
+  @GetMapping("/customLogin")
+  public void loginInput(String error, String logout, Model model) {
+
+    log.info("error: " + error);
+
+    log.info("logout : " + logout);
+
+    if (error != null) {
+      model.addAttribute("error", "로그인 인증이 실패하였습니다.");
+    }
+
+    if (logout != null) {
+      model.addAttribute("logout", "로그아웃 되었습니다.");
+    }
+  }
+
 }
