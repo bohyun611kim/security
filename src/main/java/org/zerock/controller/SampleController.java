e8b94950d6883f75861b8c1ba1d3dbc6549b8506
tomdanbi <bohyun611.kim@gmail.com>
2021-12-20 12:32:14 +0900

    시큐리티가 필요한 설계

--- src/main/java/org/zerock/controller/SampleController.java
+++ src/main/java/org/zerock/controller/SampleController.java
@@ -1,7 +1,5 @@
 package org.zerock.controller;
 
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,21 +33,6 @@ public class SampleController {
     log.info("admin only");
   }  
   
-  
-  @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MEMBER')")
-  @GetMapping("/annoMember")
-  public void doMember2() {
-    
-    log.info("logined annotation member");
-  }
-  
-  
-  @Secured({"ROLE_ADMIN"})
-  @GetMapping("/annoAdmin")
-  public void doAdmin2() {
-
-    log.info("admin annotaion only");
-  }
-
+ 
   
 }
